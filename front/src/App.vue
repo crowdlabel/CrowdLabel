7da72ec3de05640e9c1dc1cd63c8d8b814493c24
KennyL0423 <kennyliang2002@163.com>
2022-10-29 01:31:20 +0800

    front end update
    main fonts uploaded, first page update

--- front/src/App.vue
+++ front/src/App.vue
@@ -9,12 +9,15 @@
 </template>
 
 <style>
+@import '@/assets/font/font.css';
+body{
+  margin: 0 !important;
+}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
 }
 
 nav {
--- front/src/views/HomeView.vue
+++ front/src/views/HomeView.vue
@@ -1,7 +1,15 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="homepage">
+    <el-container id="main">
+      <el-header id="topnav">
+        <h2 class="logo">CrowdLabel</h2>
+        <p class="chinese_logo">| 众包平台</p>
+        <el-button type="primary" round id="login">login/register</el-button>
+      </el-header>
+      <el-main id="mid">
+        <img id="main_background" alt="Vue logo" src="../assets/crowdlabel_templogo2.jpg">
+      </el-main>
+    </el-container>
   </div>
 </template>
 
@@ -10,15 +18,61 @@
 // import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
-  name: 'HomeView',
-  components: {
-    // HelloWorld
-  }
+  // name: 'HomeView',
+  // components: {
+  //   // HelloWorld
+  // }
 }
 </script>
 
 <style scoped>
-h1,h2{
-  font-weight: normal;
+@import '@/assets/font/font.css';
+*{
+  padding: 0px !important;
+  border: 0px !important;
+  margin: 0px !important;
+}
+body{
+  margin-top: 0px !important;
+  margin-bottom: 0px !important;
+  margin-left: 0px !important;
+  margin-right: 0px !important;
+}
+.logo{
+  width: 150px;
+  height: 40px;
+  position: relative;
+  top: 12px;
+  left: 30px;
+  font-family: 'Lobster';
+  font-size: 32px;
+  color:white;
+}
+.chinese_logo{
+  width:200px;
+  height:40px;
+  position:relative;
+  top: -24px;
+  left: 140px;
+  color: white;
+  font-family: 'YouSheBiaoTiHei';
+  font-size: 24px;
+}
+#login{
+  width: 120px;
+  height: 28px;
+  position:relative;
+  top: -63px;
+  left: 450px;
+  font-size: 16px;
+  color: white;
+  background-color: #5D3BE6;
+}
+#topnav{
+  background-color: #3C1C66;
+}
+#main_background{
+  width:100%;
+  height:450px;
 }
 </style>
