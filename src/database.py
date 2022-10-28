444de8960fb790501237d9d0ab5c02eb68c89329
George Tian <github@georgetian.com>
2022-10-29 01:18:19 +0800

    email

--- src/database.py
+++ src/database.py
@@ -1,6 +1,5 @@
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy import Column, Integer, String
 Base = declarative_base()
 
 from checkers.user import MAX_USERNAME_LENGTH
