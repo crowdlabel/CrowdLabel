5093d6c75fe17a6f27760b95994968740d6dea62
KennyL0423 <kennyliang2002@163.com>
2022-10-30 16:26:00 +0800

    frontpage login dialog update
    front page login dialog

--- front/src/views/HomeView.vue
+++ front/src/views/HomeView.vue
@@ -4,14 +4,36 @@
       <el-header id="topnav">
         <h2 class="logo">CrowdLabel</h2>
         <p class="chinese_logo">| 众包平台</p>
-        <el-button type="primary" round id="login">注册/登录</el-button>
+        <el-button type="primary" @click="chooseType" id="login">注册/登录</el-button>
       </el-header>
+
+      <el-dialog
+          :visible.sync="dialogVisible"
+          :before-close="handleClose"
+          width="50%"
+          class="dialogClass"
+          border-radius="12px">
+          <div class="type_page">
+            <div class="type_title">
+              <h2 id="you_are">请问您是？</h2>
+            </div>
+            <div class="type_select">
+              <img src="../assets/sender.png" alt="任务发送方" id="image_sender">
+              <p class="chinese_type" id="type_or">或</p>
+              <img src="../assets/receiver.png" alt="任务接收方" id="image_receiver">
+              <p class="chinese_type" id="chinese_sender">任务发送方</p>
+              <p class="chinese_type" id="chinese_receiver">任务接收方</p>
+              <el-button class="login_btn" id="sender" @click="senderType"></el-button>
+              <el-button class="login_btn" id="receiver" @click="receiverType"></el-button>
+            </div>
+          </div>
+        </el-dialog>
 
       <el-main id="mid">
         <div class="maintop">
           <div class="mainimage"></div>
           <h1 id="front_big">CrowdLabel</h1>
-          <el-button type="primary" round id="getstarted">开始体验</el-button>
+          <el-button type="primary" round @click="chooseType" id="getstarted">开始体验</el-button>
           <div class="smalltriangle">
           </div>
         </div>
@@ -31,10 +53,16 @@
 // import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
-  // name: 'HomeView',
-  // components: {
-  //   // HelloWorld
-  // }
+  data() {
+    return {
+      dialogVisible: false
+    };
+  },
+  methods: {
+    chooseType: function () {
+      this.dialogVisible = true
+    }
+  }
 }
 </script>
 
@@ -173,5 +201,83 @@ body{
   top: 90px;
   font-family: 'SourceHanSansSC';
   font-size: 18px;
+}
+::v-deep .dialogClass .el-dialog{
+  width: 50%;
+  min-width: 400px;
+  border-radius: 12px;
+}
+::v-deep .el-dialog__body{
+  padding: 0;
+}
+
+
+.sender{
+  width:40px;
+  height:40px;
+  border-color: transparent;
+  color: transparent;
+}
+.receiver{
+  width: 40px;
+  height: 40px;
+  border-color: transparent;
+  color: transparent;
+}
+#image_sender{
+  float:left;
+  top: 40px;
+  left: 30px;
+  width: 40%;
+  position: relative;
+}
+#image_receiver{
+  top: 40px;
+  right: 30px;
+  float:right;
+  width: 40%;
+  position:relative;
+}
+.type_page{
+  width: 100%;
+  height: 400px;
+  position:relative;
+  top: 0px;
+}
+.chinese_type{
+  float:left;
+  font-family: "PangMenZhengDaoBiaoTi";
+  font-size: 22px;
+  width:120px;
+}
+#type_or{
+  position: relative;
+  width: 40px;
+  top: 40%;
+  left:6%;
+  font-size: 28px;
+}
+.type_select{
+  width:100%;
+  height: 350px;
+}
+#chinese_sender{
+  position:relative;
+  left: 15%;
+  top: 40px;
+}
+#chinese_receiver{
+  float: right;
+  position: relative;
+  right:15%;
+  top: 40px;
+}
+.login_btn{
+  position: relative;
+  width: 40%;
+  height: 40vw;
+  background: transparent;
+  color: transparent;
+  border-color: transparent;
 }
 </style>
