081e1c60003b3967b7c0962fc1961accd451c37e
George Tian <github@georgetian.com>
2022-11-18 13:57:13 +0800

    main

--- src/backend/main.py
+++ src/backend/main.py
@@ -7,6 +7,5 @@ from utils.hasher import hash
 
 
 if __name__ == '__main__':
-    print(len(hash('testsdsadasdfffff0230fj903j)(@*$)(@HF)(@J(ffffffffffffffffffffasdf')))
-    #uvicorn.run('main:app', host='0.0.0.0', port=8000, reload=False)
+    uvicorn.run('main:app', host='0.0.0.0', port=8000, reload=False)
     # to view API documentation, visit 127.0.0.1:8000/docs
