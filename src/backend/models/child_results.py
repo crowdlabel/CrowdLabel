305704b99f55373afd94f998a3eda3bfd1e9dece
chenjz20 <843273746@qq.com>
2022-11-19 14:02:20 +0800

    result database

---
+++ src/backend/models/child_results.py
@@ -0,0 +1,16 @@
+from sqlalchemy import Column, Integer, String ,ForeignKey
+from sqlalchemy.ext.declarative import declarative_base
+Base = declarative_base()
+
+
+MAX_RESULT_LENGTH = 512
+
+class ChildResults(Base):
+    __tablename__ = 'childResult'
+    id = Column(Integer, unique=True, primary_key=True)
+    response = Column(String(MAX_RESULT_LENGTH))
+    result_id = Column(Integer,ForeignKey('result.id'))
+    def __init__(self, id, response,result_id):
+        self.id = id 
+        self.response = response
+        self.result_id = result_id
---
+++ src/backend/models/results.py
@@ -0,0 +1,21 @@
+from sqlalchemy import Column, Integer, String ,DateTime
+from sqlalchemy.ext.declarative import declarative_base
+Base = declarative_base()
+
+
+MAX_USERNAME_LENGTH = 64
+HASH_LENGTH = 97 # len(hash('test'))
+MAX_EMAIL_LENGTH = 320 # RFC
+VERIFICATION_CODE_LENGTH = 6
+
+class Results(Base):
+    __tablename__ = 'result'
+    id = Column(Integer,unique=True,primary_key = True)
+    name = Column(String(MAX_USERNAME_LENGTH))
+    date_created = Column(DateTime)
+    date_download = Column(DateTime)
+    def __init__(self, id, name,date_created,date_download):
+        self.id = id 
+        self.name = name 
+        self.date_created = date_created
+        self.date_download = date_download
