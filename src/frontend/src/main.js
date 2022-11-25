ddb150b90ccd896d2e4acf0d7c9ce2165634288e
George Tian <github@crowdlabel.org>
2022-11-25 13:50:39 +0800

    merge dev

---
+++ src/frontend/src/main.js
@@ -0,0 +1,19 @@
+import Vue from 'vue'
+import App from './App.vue'
+import router from './router'
+import store from './store'
+import Element from "element-ui"
+import 'element-ui/lib/theme-chalk/index.css'
+import axios from 'axios'
+import VueAxios from 'vue-axios'
+
+Vue.prototype.$axios = axios;
+Vue.use(Element)
+Vue.use(VueAxios, axios)
+Vue.config.productionTip = false
+
+new Vue({
+  router,
+  store,
+  render: h => h(App)
+}).$mount('#app')
---
+++ src/frontend/src/views/SenderLogin.vue
@@ -0,0 +1,17 @@
+<template>
+<body id="sendlogin">
+    <div class="login_page">
+        <h1>你好</h1>
+    </div>
+</body>
+</template>
+
+<script>
+    export default {
+    };
+</script>
+
+<style scoped>
+
+</style>
+
