7ae5657dbd6ec54806122bee0dc759a87dca208a
chenjz20 <843273746@qq.com>
2022-12-14 16:50:17 +0800

    update

--- src/backend/models/task.py
+++ src/backend/models/task.py
@@ -1,25 +1,29 @@
-from sqlalchemy import Column, Integer, String ,DateTime
+from sqlalchemy import Column, Integer, String ,DateTime,Float
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship
 from .basicbase import Base
 from .user2task import *
+from .task2question import *
 import datetime
 MAX_NAME_LENGTH = 64
 MAX_DETAIL_LENGTH = 512
 MAX_PATH_LENGTH =128
+MAX_TAGS_LENGTH = 256
 class Task(Base):
     __tablename__ = 'task'
     id = Column(Integer,unique = True, primary_key=True,autoincrement=True)
-    type = Column(Integer)
     creator = Column(String(MAX_NAME_LENGTH))
     introduction =Column(String(MAX_NAME_LENGTH))
-    details  = Column(String(MAX_DETAIL_LENGTH))
+    description  = Column(String(MAX_DETAIL_LENGTH))
     name = Column(String(MAX_NAME_LENGTH))
-    path = Column(String(MAX_PATH_LENGTH))
+    cover_path = Column(String(MAX_PATH_LENGTH))
     date_created = Column(DateTime,default = datetime.datetime.now)
-
-    questions = relationship('Question')
+    credits = Column(Float)
+    tags = Column(String(MAX_TAGS_LENGTH))
+    response_required = Column(Integer)
+    
     results = relationship('Results')
+    questions = relationship('Question',secondary=Task2Question)
     requester = relationship('Requester',secondary=Requester2Task)
     respondent_claimed = relationship('Respondent',secondary=Respondent2Claim)
     respondent_complete = relationship('Respondent',secondary = Respondent2Complete)
---
+++ src/backend/models/task2question.py
@@ -0,0 +1,9 @@
+from sqlalchemy import Column, Integer,ForeignKey,UniqueConstraint
+from .basicbase import Base
+class Task2Question(Base):
+    __tablename__ = 'task2question'
+    id = Column(Integer,primary_key=True)
+    task = Column(Integer,ForeignKey('task.id'))
+    question = Column(Integer,ForeignKey('question.id'))
+    UniqueConstraint('task','question',name='task2question_relation')
+
