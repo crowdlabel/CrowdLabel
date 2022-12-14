eff3e993e21e605f7044170d144d24b939b21202
chenjz20 <843273746@qq.com>
2022-12-14 16:22:44 +0800

    task 2 user

--- src/backend/models/task.py
+++ src/backend/models/task.py
@@ -2,6 +2,8 @@ from sqlalchemy import Column, Integer, String ,DateTime
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship
 from .basicbase import Base
+from .user2task import *
+import datetime
 MAX_NAME_LENGTH = 64
 MAX_DETAIL_LENGTH = 512
 MAX_PATH_LENGTH =128
@@ -14,8 +16,14 @@ class Task(Base):
     details  = Column(String(MAX_DETAIL_LENGTH))
     name = Column(String(MAX_NAME_LENGTH))
     path = Column(String(MAX_PATH_LENGTH))
+    date_created = Column(DateTime,default = datetime.datetime.now)
+
     questions = relationship('Question')
     results = relationship('Results')
+    requester = relationship('Requester',secondary=Requester2Task)
+    respondent_claimed = relationship('Respondent',secondary=Respondent2Claim)
+    respondent_complete = relationship('Respondent',secondary = Respondent2Complete)
+   
     def __init__(self,name,creator,detail,introduction,type,path) -> None:
         self.name=name
         self.creator = creator
--- src/backend/models/user.py
+++ src/backend/models/user.py
@@ -1,24 +1,56 @@
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, Integer, String ,DateTime,FLOAT,ForeignKey
 from sqlalchemy.ext.declarative import declarative_base
 from .basicbase import Base
-
+from sqlalchemy.orm import relationship
+from .user2task import *
+import datetime
 
 MAX_USERNAME_LENGTH = 64
 HASH_LENGTH = 97 # len(hash('test'))
 MAX_EMAIL_LENGTH = 320 # RFC
 VERIFICATION_CODE_LENGTH = 6
-
+MAX_USERTYPE_LENGTH = 20
 class User(Base):
     __tablename__ = 'user'
-    username = Column(String(MAX_USERNAME_LENGTH), unique=True, primary_key=True)
+    id = Column(Integer,primary_key = True,autoincrement=True)
+    username = Column(String(MAX_USERNAME_LENGTH), unique=True)
     password_hashed = Column(String(HASH_LENGTH))
     email = Column(String(MAX_EMAIL_LENGTH))
-    user_type = Column(Integer)
-    status = Column(Integer)
-
+    user_type = Column(String(MAX_USERTYPE_LENGTH))
+    date_created = Column(DateTime,default = datetime.datetime.now)
+    tokens = Column(FLOAT)
+    __mapper_args__ = {
+        'polymorphic_on':type,
+        'polymorphic_identity':'user'
+    }
     def __init__(self, username, password_hashed, email, user_type, status):
         self.username = username
         self.password_hashed = password_hashed
         self.email = email
         self.user_type = user_type
         self.status = status
+
+class Requester(User):
+    __tablename__ = 'requester'
+    id = Column(Integer,ForeignKey('user.id'),primary_key = True)
+    task_requested = relationship('Requester2Task',secondary=Requester2Task)
+    __mapper_args__ = {
+        'polymorphic_on':type,
+        'polymorphic_identity':'requester'
+    }
+class Respondent(User):
+    __tablename__ = 'respondent'
+    id = Column(Integer,ForeignKey('user.id'),primary_key = True)
+    task_claimed = relationship('Respondent2Claim',secondary=Respondent2Claim)
+    task_complete = relationship('Respondent2Complete',secondary=Respondent2Complete)
+    __mapper_args__ = {
+        'polymorphic_on':type,
+        'polymorphic_identity':'respondent'
+    }
+class Admin(User):
+    __tablename__ = 'Admin'
+    id = Column(Integer,ForeignKey('user.id'),primary_key = True)
+    __mapper_args__ = {
+        'polymorphic_on':type,
+        'polymorphic_identity':'admin'
+    }
