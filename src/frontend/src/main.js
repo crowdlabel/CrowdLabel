3849362416cc823c176bfc0f138ffdd8a937b205
KennyL0423 <kennyliang2002@163.com>
2022-12-21 14:48:05 +0800

    authorization updates

--- src/frontend/src/main.js
+++ src/frontend/src/main.js
@@ -17,3 +17,14 @@ new Vue({
   store,
   render: h => h(App)
 }).$mount('#app')
+
+axios.interceptors.request.use(
+  config => {
+    if (localStorage.getItem('Authorization')) {
+      config.headers.Authorization = localStorage.getItem('Authorization');
+    }
+    return config;
+  }, error => {
+    return Promise.reject(error);
+  }
+);
