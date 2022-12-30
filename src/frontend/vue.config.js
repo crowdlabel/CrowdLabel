53ab38107c9f6992581e95450323b72bb1f428da
AmyXin <amyxx2001@163.com>
2022-12-30 20:09:16 +0800

    连答题2.5

--- src/frontend/vue.config.js
+++ src/frontend/vue.config.js
@@ -1,17 +1,6 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
-  lintOnSave:false,
-  devServer: {
-    proxy: {
-        '/api': {
-            target: 'http://localhost:8000',
-            changeOrigin: true,
-            pathRewrite: {
-                '/api': ''
-            }
-        }
-    }
-  }
+  lintOnSave:false
 })
 
