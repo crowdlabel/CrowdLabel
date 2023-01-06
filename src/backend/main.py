741401c3f69c671a41eff0845f3af46def9bc565
George Tian <github@georgetian.com>
2023-01-06 16:02:45 +0800

    update

--- src/backend/main.py
+++ src/backend/main.py
@@ -10,15 +10,21 @@ args = parser.parse_args()
 import utils.config
 utils.config.load_config(args.config)
 
-if args.init_db:
-    from services.database import init_models_sync
-    init_models_sync()
-elif args.docs:
-    import controllers.routers
-    controllers.routers.generate_docs()
-else:
-    root_path = ''
-    if args.prod:
-        root_path = '/api'
-    import uvicorn
-    uvicorn.run('controllers.routers:app', host='0.0.0.0', port=8000, reload=False, root_path=root_path)
+
+def main():
+
+    if args.init_db:
+        from services.database import init_models_sync
+        init_models_sync()
+    elif args.docs:
+        import controllers.routers
+        controllers.routers.generate_docs()
+    else:
+        root_path = ''
+        if args.prod:
+            root_path = '/api'
+        import uvicorn
+        uvicorn.run('controllers.routers:app', host='0.0.0.0', port=8000, reload=False, root_path=root_path)
+
+if __name__ == '__main__':
+    main()
