a0f1f13a17f1adb22e8bc74d846ad95392809451
chenjz20 <843273746@qq.com>
2023-01-06 17:14:50 +0800

    update

--- src/backend/services/questions.py
+++ src/backend/services/questions.py
@@ -104,18 +104,19 @@ class Questions:
         if target_answer == None:
             if isinstance(answer,schemas.answers.MultiChoiceAnswer):
                 if target.question_type != 'multi_choice':
+                    await asyncio.shield(con.close())
                     return f'type mismatch , question type {target.question_type} ,answer type multi_choice'
                 new_answer = models.answer.MultiChoiceAnswer()
-                print(answer)
-                print(1)
                 new_answer.choices = '|'.join([str(choice) for choice in answer.choices])
             elif isinstance(answer,schemas.answers.SingleChoiceAnswer):
                 if target.question_type != 'single_choice':
+                    await asyncio.shield(con.close())
                     return f'type mismatch , question type {target.question_type} ,answer type single_choice'
                 new_answer = models.answer.SingleChoiceAnswer()
                 new_answer.choice = answer.choice
             elif  isinstance(answer,schemas.answers.BoundingBoxAnswer):
                 if target.question_type != 'bounding_box':
+                    await asyncio.shield(con.close())
                     return f'type mismatch , question type {target.question_type} ,answer type bounding_box'
                 new_answer = models.answer.BoundingBoxAnswer()
                 for corner in answer.boxes:
@@ -129,6 +130,7 @@ class Questions:
                     new_answer.corner.append(new_corner)
             elif  isinstance(answer,schemas.answers.OpenAnswer):
                 if target.question_type != 'open':
+                    await asyncio.shield(con.close())
                     return f'type mismatch , question type {target.question_type} ,answer type open'
                 new_answer = models.answer.OpenAnswer()
                 new_answer.text = answer.text
