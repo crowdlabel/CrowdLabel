65e77a311465e06ba74995d1376a2a56074d8b3b
KennyL0423 <kennyliang2002@163.com>
2023-01-07 21:25:47 +0800

    updates

--- src/frontend/src/views/ReceiverLogin.vue
+++ src/frontend/src/views/ReceiverLogin.vue
@@ -213,6 +213,9 @@ export default {
             this.time = time
             this.verifyEmailbtn();
         }
+        
+    },
+    mounted () {
         let base = this.$root.basePath
         var apiClient = new ApiClient(base);
         this.client = apiClient
@@ -220,9 +223,6 @@ export default {
         this.user = usersApi
         var authApi = new AuthApi(apiClient);
         this.auth = authApi
-    },
-    mounted () {
-        
     },
     methods: {
         handleTabClick(tab, event){
