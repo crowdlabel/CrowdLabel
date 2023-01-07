1e34bc043f1b62d76a2c0f2b3614ede8cf9baa17
KennyL0423 <kennyliang2002@163.com>
2023-01-07 17:13:49 +0800

    checked basePath
    now we only need to change basePath in main.js to fit whatever server we are using

--- src/frontend/src/views/ReceiverLogin.vue
+++ src/frontend/src/views/ReceiverLogin.vue
@@ -213,7 +213,8 @@ export default {
             this.time = time
             this.verifyEmailbtn();
         }
-        var apiClient = new ApiClient('http://localhost:8000');
+        let base = this.$root.basePath
+        var apiClient = new ApiClient(base);
         this.client = apiClient
         var usersApi = new UsersApi(apiClient);
         this.user = usersApi
--- src/frontend/src/views/SenderLogin.vue
+++ src/frontend/src/views/SenderLogin.vue
@@ -215,7 +215,8 @@ export default {
             this.time = time
             this.verifyEmailbtn();
         }
-        var apiClient = new ApiClient('http://localhost:8000');
+        let base = this.$root.basePath
+        var apiClient = new ApiClient(base);
         this.client = apiClient
         var usersApi = new UsersApi(apiClient);
         this.user = usersApi
